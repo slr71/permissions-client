8134a9d24daa6e1fef7b781c98d8e66f228aa6a3
Sarah Roberts <sarahr@cyverse.org>
2023-02-14 13:30:08 -0700

    CORE-1875: updated dependencies and fixed a lint warning

--- project.clj
+++ project.clj
@@ -2,16 +2,16 @@
   :description "A Clojure client library for the CyVerse permissions service."
   :url "https://github.com/cyverse-de/permissions-client"
   :license {:name "BSD"
-            :url "http://iplantcollaborative.org/sites/default/files/iPLANT-LICENSE.txt"}
+            :url "https://cyverse.org/license"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :plugins [[jonase/eastwood "0.4.2"]
+  :plugins [[jonase/eastwood "1.3.0"]
             [lein-ancient "0.7.0"]
-            [test2junit "1.2.2"]]
-  :dependencies [[cheshire "5.10.0"]
-                 [clj-http "3.12.1"]
+            [test2junit "1.4.4"]]
+  :dependencies [[cheshire "5.11.0"]
+                 [clj-http "3.12.3"]
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
-                 [medley "1.3.0"]
-                 [org.clojure/clojure "1.10.3"]]
+                 [medley "1.4.0"]
+                 [org.clojure/clojure "1.11.1"]]
   :profiles {:dev {:dependencies [[clj-http-fake "1.0.3"]]}
              :repl {:source-paths ["repl"]}})
