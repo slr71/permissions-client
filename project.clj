33d206c6658cae89a47ae1ff9ce2162b5d7cdac0
Sarah Roberts <sarahr@cyverse.org>
2023-02-15 14:51:31 -0700

    Version 2.8.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.cyverse/permissions-client "2.8.3-SNAPSHOT"
+(defproject org.cyverse/permissions-client "2.8.3"
   :description "A Clojure client library for the CyVerse permissions service."
   :url "https://github.com/cyverse-de/permissions-client"
   :license {:name "BSD"
