465647c088b7215855d23a171850adc536b3e6f5
Sarah Roberts <sonoran.sarah@gmail.com>
2024-07-23 18:41:10 -0700

    CORE-2006: updated dependencies and .gitignore

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "https://cyverse.org/license"}
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :plugins [[jonase/eastwood "1.3.0"]
+  :plugins [[jonase/eastwood "1.4.3"]
             [lein-ancient "0.7.0"]
             [test2junit "1.4.4"]]
-  :dependencies [[cheshire "5.11.0"]
-                 [clj-http "3.12.3"]
+  :dependencies [[cheshire "5.13.0"]
+                 [clj-http "3.13.0"]
                  [com.cemerick/url "0.1.1" :exclusions [com.cemerick/clojurescript.test]]
-                 [com.github.seancorfield/honeysql "2.4.979"]
+                 [com.github.seancorfield/honeysql "2.6.1147"]
                  [medley "1.4.0"]
-                 [org.clojure/clojure "1.11.1"]]
-  :profiles {:dev {:dependencies [[clj-http-fake "1.0.3"]]}
+                 [org.clojure/clojure "1.11.3"]]
+  :profiles {:dev {:dependencies [[clj-http-fake "1.0.4"]]}
              :repl {:source-paths ["repl"]}})
